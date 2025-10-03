4155960cd3d7af9cc19ef2e869f973d15a9c51c8
shehbaz <mdshehbaz666666@gmail.com>
2025-10-03 13:47:56 +0530

    feat: Update CORS configuration to use environment variable for origin

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 const app =express();
 
 app.use(cors({
-    origin : ["https://knit-finance-assignment.vercel.app"],
+    origin : process.env.CORS_ORIGIN,
     methods:["GET","HEAD","PUT","PATCH","POST","DELETE"],
     credentials:true,
 }))
