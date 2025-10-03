301973f2efa48e2b495207e1292f53083ef65939
shehbaz <mdshehbaz666666@gmail.com>
2025-10-03 13:39:35 +0530

    feat: Update CORS configuration to allow requests from the production URL

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 const app =express();
 
 app.use(cors({
-    origin : ["http://localhost:5173"],
+    origin : ["https://knit-finance-assignment.vercel.app"],
     methods:["GET","HEAD","PUT","PATCH","POST","DELETE"],
     credentials:true,
 }))
